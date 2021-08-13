7171cf47e35efc5f51ba1f3357eee3b235bc5071
Andy <68723584+LovelyAndy@users.noreply.github.com>
2021-08-13 15:33:19 -0500

    fontawesome added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+library.add(fas, fab)
+
+// App.config.productionTip = false
+createApp(App)
+  .component('fa', FontAwesomeIcon)
+  .mount('#app')
